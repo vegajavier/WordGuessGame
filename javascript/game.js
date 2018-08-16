738a139e75180b330f8d37dcdfdf3c7aac90b5b5
vegajavier <vegajavier86@gmail.com>
2018-08-15 22:50:16 -0500

    js and css additions

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,44 @@
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  
+// choosing a random word //
+//create an array for turtle words//
+const words=[
+        "leonardo" , "donattello" , "rafael" , "michaelangelo" , "splinter" , "shredder" , "april oniel" , "casey jones" , "rock steady" , "bebop"
+];
+let wins=0;
+let loss=0;
+let wrongGuess = 0;
+const guessesRemaining= 8;
+const attempts = 8;
+let userGuesses = [];
+let underScores = "";
+let shuffleWords;
+const dash="_  ";
+let newGame;
+
+// create dashes for length of words//
+function playGame(){
+        
+        shuffleWords = words[Math.floor(Math.random() * words.length)];
+       
+console.log(shuffleWords);
+
+let len=shuffleWords.length;
+underScores=dash.repeat(len);
+
+console.log(underScores);
+}
+
+//start game test////
+console.log(playGame());
+
+document.querySelector(".underScorescontainer").innerHTML = underScores;
+//
+// keycode =o;
+// arr=[length,e,o,r,o,da,o,]
+// for(i=0;i<arr.length;i++){
+//         if(kecode===arr[i]){
+//                 get indexedDB,
+//                 i,3,6
+// =>o        }
+// }
+// --0--0
+
