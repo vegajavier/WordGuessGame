e8cefac83c04600ed4d933fca7c2624c48f9990d
vegajavier <vegajavier86@gmail.com>
2018-08-18 07:21:11 -0500

    not completed

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,6 @@
 /// array of ninja turtles character names
 const words = [
-        "leonardo", "donattello", "rafael", "michaelangelo", "splinter", "shredder", "april oniel", "casey jones", "rock steady", "bebop"
+	"leonardo", "donattello", "rafael", "michaelangelo", "splinter", "shredder", "april oniel", "casey jones", "rock steady", "bebop"
 ];
 ///// generate random word with underscores
 // create dashes for length of words//
@@ -10,49 +10,43 @@ const dash = "_ ";
 let guessesLeft = 8;
 let userGuesses = [];
 function newGame() {
-        shuffleWords = words[Math.floor(Math.random() * words.length)];
-        console.log(shuffleWords);
-        let len = shuffleWords.length;
-        underScores = dash.repeat(len).split("");
-        console.log(underScores);
-
-      
-        document.querySelector(".underScorescontainer").innerHTML = underScores;
-        
-};
+	shuffleWords = words[Math.floor(Math.random() * words.length)];
+	console.log(shuffleWords);
+	let len = shuffleWords.length;
+	underScores = dash.repeat(len).split(" ");
+	console.log(underScores);
+	
+	document.querySelector(".underScorescontainer").innerHTML = underScores;
+}
 document.addEventListener("keypress", (event) => {
-        console.log(event.key);
-        let userletter = event.key //key user typed
-        let inputlower = (userletter.toLowerCase());
-        if (guessesLeft !== 0) {
-                userGuesses.push(inputlower);
-                //getting index of userinput against shuffle word;
-                let indexes = []; //taking empty string to store positions.
-                for (i = 0; i < shuffleWords.length; i++) //if 
-                {
-                        if (shuffleWords[i] == inputlower)
-                                indexes.push(i); //to push input to word if both characters are same.
-                               
-                }
-                console.log(indexes);
+	console.log(event.key);
+	let userletter = event.key //key user typed
+	let inputlower = (userletter.toLowerCase());
+	if (guessesLeft !== 0) {
+			userGuesses.push(inputlower);
+			//getting index of userinput against shuffle word;
+			let indexes = []; //taking empty string to store positions.
+			for (let i = 0; i < shuffleWords.length; i++) {
+				if (shuffleWords[i] === inputlower)
+						indexes.push(i); //to push input to word if both characters are same.
+						console.log(indexes);
+				}
+			
+				for (let i = 0; i < indexes.length; i++) {
+					let specificIndex = indexes[i];
+					underScores[specificIndex] = userletter;
+				}
+	  
+			console.log(underScores)
+			document.querySelector(".underScorescontainer").innerHTML = userGuesses;
 
-                for (let i = 0; i < indexes.length; i++) {
-                        let specificIndex = indexes[i];
-                        underScores[specificIndex] = userletter;
-                }
-          
-                console.log(underScores)
-                document.querySelector(".underScorescontainer").innerHTML = userGuesses;
+			
 
-                
-
-                let correct = [];
-                let incorrect = [];
+			let correct = [];
+			let incorrect = [];
 
 
 
-        }
-
+	}
 })
-
 newGame()
